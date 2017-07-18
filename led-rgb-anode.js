da76d4849ed6b03026f3613a0ddd79639a0789b9
RyoKojima <ryokojima@kojima.com>
2017-07-18 18:52:07 +0900

    rainbow

--- led-rgb-anode.js
+++ led-rgb-anode.js
@@ -11,15 +11,18 @@ board.on("ready", function() {
     isAnode: true
   });
 
+  var index = 0;
+  var rainbow = ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "8F00FF"];
+
   // Add led to REPL (optional)
   this.repl.inject({
     anode: anode
   });
 
-  // Turn it on and set the initial color
-  anode.on();
-  anode.color("#FF0000");
-
-  anode.blink(1000);
-
+  this.loop(1000, function() {
+    anode.color(rainbow[index++]);
+    if (index === rainbow.length) {
+      index = 0;
+    }
+  });
 });
